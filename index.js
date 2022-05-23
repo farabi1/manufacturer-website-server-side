35db5897dd71a04b5fd65f16b07a4f5a277a7bce
farabi1 <rashidfarabi@gmail.com>
2022-05-23 11:20:32 +0600

    Server Setup - MongoDB

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ll96.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Manufacturer DB Connected");
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Manufacturer Web App')
