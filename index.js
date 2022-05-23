1bde8f67d6ad13a2bea68bef91e7a590db191dd4
farabi1 <rashidfarabi@gmail.com>
2022-05-23 11:13:13 +0600

    Server Setup - MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ll96.mongodb.net/?retryWrites=true&w=majority`;
 
 
 app.get('/', (req, res) => {
