e148034a4dc2b9e3640fcb383588ff2303b129fc
farabi1 <rashidfarabi@gmail.com>
2022-05-26 03:29:53 +0600

    Client Setup - Manage product Done

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
         })
 
 
+        app.delete('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await partsCollection.deleteOne(query);
+            res.send(result)
+        })
+
+
 
         app.get('/reviews', async (req, res) => {
             const query = {};
