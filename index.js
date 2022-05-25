843cd66233884d8c88e4ca0c75fce1fb44a28426
farabi1 <rashidfarabi@gmail.com>
2022-05-25 16:08:46 +0600

    Server Setup - JWT Not Working Properly

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next) {
-    const authorHeader = req.headers.authorization;
-    if (!authorHeader) {
-        return res.status(401).send({ message: 'Access Not Allowed' });
-    }
-    const token = authorHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: 'No Access' })
-        }
-        req.decoded = decoded;
-        next();
-    });
-}
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'Access Not Allowed' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'No Access' })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     });
+// }
 
 
 async function run() {
@@ -69,14 +69,11 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/orders', verifyJWT, async (req, res) => {
+        app.get('/orders', async (req, res) => {
             const customer = req.query.customerMail;
-
-
             const query = { customer: customer };
             const orders = await ordersCollection.find(query).toArray();
             res.send(orders);
-
         })
 
         app.post('/orders', async (req, res) => {
